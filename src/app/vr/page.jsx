aa6f717fa95e38ca12a8fb7cb18c4533fe687919
Shekharkumar07 <shekharkr14th@gmail.com>
2026-06-30 15:28:58 +0530

    Refactor VRPage to use async searchParams

--- src/app/vr/page.jsx
+++ src/app/vr/page.jsx
@@ -1,20 +1,8 @@
-'use client'
-
-import { Suspense } from 'react'
-import { useSearchParams } from 'next/navigation'
 import VRWrapper from '../components/VRWrapper'
 
-function VRContent() {
-  const searchParams = useSearchParams()
-  const image = searchParams.get('img') || '20067.jpg'
+export default async function VRPage({ searchParams }) {
+  const params = await searchParams
+  const image = params?.img || '20067.jpg'
 
   return <VRWrapper imageUrl={`/${image}`} />
-}
-
-export default function VRPage() {
-  return (
-    <Suspense fallback={<div>Loading VR...</div>}>
-      <VRContent />
-    </Suspense>
-  )
 }
